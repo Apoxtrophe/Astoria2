69688e2eef009eeecaae6b1831d711a9215f4b3a
Apoxtrophe <cyggardner@gmail.com>
2023-11-18 08:31:31 -0700

    Basic Player Controller

---
+++ src/config.rs
@@ -0,0 +1,12 @@
+//Configuration File
+pub const PLAYER_HEIGHT: f32 = 2.0;
+pub const PLAYER_WIDTH: f32 = 1.0;
+
+pub const WORLD_SIZE: f32 = 100.0;
+
+pub const MOUSE_SENSITIVITY: f32 = 0.001;
+pub const MAX_PITCH: f32 = std::f32::consts::FRAC_PI_2 - 0.1; // Slightly less than 90 degrees
+pub const MIN_PITCH: f32 = -std::f32::consts::FRAC_PI_2 + 0.1; // Slightly more than -90 degrees
+
+pub const SCREEN_WIDTH: f32 = 1920.0;
+pub const SCREEN_HEIGHT: f32 = 1080.0;
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,41 @@
+mod config;
+use config::*;
 
-use bevy::prelude::*;
-use bevy::input::{Input, keyboard};
-use bevy::math::{Vec3};
+use bevy::{
+    ecs::query,
+    input::mouse::MouseMotion,
+    prelude::*,
+    window::{Cursor, CursorGrabMode, PresentMode, PrimaryWindow, WindowResolution},
+};
+
+use bevy_rapier3d::prelude::{Collider, KinematicCharacterController, KinematicCharacterControllerOutput, RigidBody};
+use bevy_rapier3d::control::{CharacterAutostep, CharacterLength};
 
 
-
-#[derive(Component)]
-struct Player;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "I am a window!".into(),
+                resolution: WindowResolution::new(SCREEN_WIDTH, SCREEN_HEIGHT),
+                present_mode: PresentMode::AutoVsync,
+                cursor: Cursor { 
+                    icon: (CursorIcon::Crosshair), 
+                    visible: (false), 
+                    grab_mode: (CursorGrabMode::Locked), 
+                    hit_test: (true) },
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
-        .add_systems(Update, draw_cursor)
-        .add_systems(Update, player_controller_system)
+        .add_systems(Startup, init_system)
+        .add_systems(Update, update_system)
+        .add_systems(Update, read_result_system)
+        .add_systems(Update, mouse_look_system)
         .run();
 }
-
-fn player_controller_system(
-    time: Res<Time>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &Player)>,
-) {
-    let speed = 5.0;
-    let delta_time = time.delta_seconds();
-
-    for (mut transform, _) in query.iter_mut() {
-        let mut direction = Vec3::ZERO;
-
-        if keyboard_input.pressed(KeyCode::W) {
-            direction.z -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::S) {
-            direction.z += 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::A) {
-            direction.x -= 1.0;
-        }
-        if keyboard_input.pressed(KeyCode::D) {
-            direction.x += 1.0;
-        }
-
-        transform.translation += speed * delta_time * direction;
-    }
-}
-
 
 #[derive(Component)]
 struct Ground;
@@ -54,25 +45,12 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // Player
-    commands.spawn(PbrBundle {
-        // Add your player's mesh, material, transform, etc.
-        ..default()
-    })
-    .insert(Player)
-    .with_children(|parent| {
-        // Camera as a child of the player
-        parent.spawn(Camera3dBundle {
-            transform: Transform::from_xyz(0.0, 2.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-            ..default()
-        });
-    });
 
 
     // plane
     commands.spawn((
         PbrBundle {
-            mesh: meshes.add(shape::Plane::from_size(20.).into()),
+            mesh: meshes.add(shape::Plane::from_size(WORLD_SIZE).into()),
             material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
             ..default()
         },
@@ -84,32 +62,76 @@ fn setup(
         transform: Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, PLAYER_HEIGHT, 0.0),
+        ..default()
+    })
+    .insert(KinematicCharacterController {
+        // Your controller configuration here
+        ..default()
+    });
 }
 
-fn draw_cursor(
-    camera_query: Query<(&Camera, &GlobalTransform)>,
-    ground_query: Query<&GlobalTransform, With<Ground>>,
-    windows: Query<&Window>,
-    mut gizmos: Gizmos,
+fn init_system(mut commands: Commands) {
+    commands.spawn((
+        RigidBody::KinematicPositionBased,
+        Collider::cuboid(PLAYER_WIDTH, PLAYER_WIDTH,PLAYER_HEIGHT),
+        KinematicCharacterController {
+            offset: CharacterLength::Relative(0.01),  // Character offset
+            up: Vec3::Z,  // Z as the up vector
+            autostep: Some(CharacterAutostep {
+                max_height: CharacterLength::Relative(0.3),
+                min_width: CharacterLength::Relative(0.5),
+                include_dynamic_bodies: true,
+            }),
+            ..default()
+        },
+    ));
+}
+
+fn update_system(mut controllers: Query<&mut KinematicCharacterController>) {
+    for mut controller in controllers.iter_mut() {
+        controller.translation = Some(Vec3::new(1.0, -0.5, 1.0));
+    }
+}
+
+fn read_result_system(controllers: Query<(Entity, &KinematicCharacterControllerOutput)>) {
+    for (entity, output) in controllers.iter() {
+        println!("Entity {:?} moved by {:?} and touches the ground: {:?}",
+                  entity, output.effective_translation, output.grounded);
+    }
+}
+
+use bevy::prelude::*;
+
+fn mouse_look_system(
+    mut mouse_motion_events: EventReader<MouseMotion>,
+    mut query: Query<(&mut Transform, &Camera)>,
+    mut current_pitch: Local<f32>, // Tracking the current pitch
 ) {
-    let (camera, camera_transform) = camera_query.single();
-    let ground = ground_query.single();
+    for (mut transform, _camera) in query.iter_mut() {
+        for event in mouse_motion_events.iter() {
+            let sensitivity_x = MOUSE_SENSITIVITY;
+            let sensitivity_y = MOUSE_SENSITIVITY;
 
-    let Some(cursor_position) = windows.single().cursor_position() else {
-        return;
-    };
+            // Calculate new pitch
+            let mut new_pitch = *current_pitch + (-event.delta.y * sensitivity_y);
+            new_pitch = new_pitch.clamp(MIN_PITCH, MAX_PITCH);
 
-    // Calculate a ray pointing from the camera into the world based on the cursor's position.
-    let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
-        return;
-    };
+            if new_pitch != *current_pitch {
+                // Apply pitch rotation only if it's within the limits
+                let pitch_diff = new_pitch - *current_pitch;
+                let pitch = Quat::from_rotation_x(pitch_diff);
+                transform.rotation = transform.rotation * pitch; // Rotate around local x axis
 
-    // Calculate if and where the ray is hitting the ground plane.
-    let Some(distance) = ray.intersect_plane(ground.translation(), ground.up()) else {
-        return;
-    };
-    let point = ray.get_point(distance);
+                *current_pitch = new_pitch; // Update the current pitch
+            }
 
-    // Draw a circle just above the ground plane at that position.
-    gizmos.circle(point + ground.up() * 0.01, ground.up(), 0.2, Color::WHITE);
+            // Yaw rotation
+            let yaw = Quat::from_rotation_y(-event.delta.x * sensitivity_x);
+            transform.rotation = yaw * transform.rotation; // Rotate around global y axis
+        }
+    }
 }
