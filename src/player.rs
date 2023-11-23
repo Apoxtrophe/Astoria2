84dd434fb212191d394d3f673b417d690ce2dad7
Apoxtrophe <cyggardner@gmail.com>
2023-11-23 10:33:29 -0700

    Is it good???

--- src/player.rs
+++ src/player.rs
@@ -14,30 +14,28 @@ use bevy_rapier3d::prelude::{Collider, KinematicCharacterController, RigidBody};
 use bevy_rapier3d::control::{CharacterAutostep, CharacterLength};
 use bevy_atmosphere::prelude::*;
 
-pub fn player_initialization_system(
-    mut commands: Commands,
-) {
-    // Build Camera
+pub fn player_initialization_system(mut commands: Commands) {
     commands.spawn(TransformBundle::default())
         .with_children(|parent| {
             parent.spawn(Camera3dBundle {
                 transform: Transform::from_xyz(0.0, PLAYER_HEIGHT, 0.0),
                 ..default()
-            })
-            .insert(AtmosphereCamera::default());
+            }).insert(AtmosphereCamera::default());
         })
-        .insert(RigidBody::KinematicPositionBased)
-        .insert(Collider::cuboid(PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_HEIGHT))
-        .insert(KinematicCharacterController {
-            offset: CharacterLength::Relative(PLAYER_OFFSET),
-            up: Vec3::Z,
-            autostep: Some(CharacterAutostep {
-                max_height: CharacterLength::Relative(AUTOSTEP_HEIGHT),
-                min_width: CharacterLength::Relative(AUTOSTEP_WIDTH),
-                include_dynamic_bodies: true,
-            }),
-            ..default()
-        });
+        .insert((
+            RigidBody::KinematicPositionBased,
+            Collider::cuboid(PLAYER_WIDTH, PLAYER_WIDTH, PLAYER_HEIGHT),
+            KinematicCharacterController {
+                offset: CharacterLength::Relative(PLAYER_OFFSET),
+                up: Vec3::Z,
+                autostep: Some(CharacterAutostep {
+                    max_height: CharacterLength::Relative(AUTOSTEP_HEIGHT),
+                    min_width: CharacterLength::Relative(AUTOSTEP_WIDTH),
+                    include_dynamic_bodies: true,
+                }),
+                ..default()
+            },
+        ));
 }
 
 pub fn player_update_system(
@@ -52,44 +50,22 @@ pub fn player_update_system(
         (keyboard_input.pressed(KeyCode::D) as i32 - keyboard_input.pressed(KeyCode::A) as i32) as f32,
         0.0,
         (keyboard_input.pressed(KeyCode::S) as i32 - keyboard_input.pressed(KeyCode::W) as i32) as f32,
-    );
+    ).normalize_or_zero() * if keyboard_input.pressed(KeyCode::ShiftLeft) { PLAYER_SPRINT_SPEED } else { PLAYER_WALK_SPEED };
 
-    let speed = if keyboard_input.pressed(KeyCode::ShiftLeft) {
-        PLAYER_SPRINT_SPEED
-    } else {
-        PLAYER_WALK_SPEED
+    let total_mouse_motion = mouse_motion_events.iter().fold(Vec2::ZERO, |acc, event| acc + event.delta);
+    let (yaw_diff, pitch_diff) = (-total_mouse_motion.x * MOUSE_SENSITIVITY, (*current_pitch - (total_mouse_motion.y * MOUSE_SENSITIVITY)).clamp(MIN_PITCH, MAX_PITCH) - *current_pitch);
+    *current_pitch += pitch_diff;
+
+    let window_center = {
+        let window = windows.single();
+        Vec2::new(window.width() / 2.0, window.height() / 2.0)
     };
 
-    // Aggregate total mouse motion for the frame
-    let mut total_mouse_motion = Vec2::ZERO;
-    for event in mouse_motion_events.iter() {
-        total_mouse_motion += event.delta;
+    for (mut transform, _) in query.iter_mut() {
+        transform.translation += transform.rotation.mul_vec3(movement_direction) * time.delta_seconds();
+        transform.rotation *= Quat::from_rotation_y(yaw_diff) * Quat::from_rotation_x(pitch_diff);
     }
 
-    for (mut transform, _camera) in query.iter_mut() {
-        let mut world_movement_direction = transform.rotation.mul_vec3(movement_direction);
-        world_movement_direction.y = 0.0;
-        if world_movement_direction.length_squared() > 0.0 {
-            let normalized_movement = world_movement_direction.normalize_or_zero() * speed;
-            transform.translation += normalized_movement * time.delta_seconds();
-        }
-
-        // Apply aggregated mouse motion
-        let new_pitch = (*current_pitch - (total_mouse_motion.y * MOUSE_SENSITIVITY)).clamp(MIN_PITCH, MAX_PITCH);
-        let pitch_diff = new_pitch - *current_pitch;
-        *current_pitch = new_pitch;
-
-        let yaw_diff = -total_mouse_motion.x * MOUSE_SENSITIVITY;
-
-        if pitch_diff != 0.0 || yaw_diff != 0.0 {
-            let pitch = Quat::from_rotation_x(pitch_diff);
-            let yaw = Quat::from_rotation_y(yaw_diff);
-            transform.rotation = yaw * transform.rotation * pitch;
-        }
-    }
-    // Center Cursor (dynamic part)
     let mut window = windows.single_mut();
-    let center_x = window.width() / 2.0;
-    let center_y = window.height() / 2.0;
-    window.set_cursor_position(Some(Vec2::new(center_x, center_y)));
+    window.set_cursor_position(Some(window_center));
 }
